6e0b4e800a29b4b471d82a65b25e60a0defe6667
Jadeed Kamal <83796140+codetoastje@users.noreply.github.com>
2021-10-10 13:16:17 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+import keyboard as kb
+import time
+
+print("Welcome to Whatsapp AutoTag\n")
+print("Enter the Number of members in your Group excluding yourself : ")
+x = int(input())
+
+print("Click on the whatsap group chatbox within 10 seconds or else u are doomed")
+time.sleep(10)
+
+i = 0
+
+while i!=x:
+    j = 0
+    kb.press("shift + @")
+    kb.release("shift + @")
+
+    while j!=i:
+        kb.press("down")
+        kb.release("down")
+        j+=1
+
+    kb.press("enter")
+    kb.release("enter")
+    i+=1
+    time.sleep(0.2)
+
+
